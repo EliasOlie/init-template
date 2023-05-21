3de328d86ee6739472fe7f7ab11ef0114a647aa0
EliasOlie <eliasvieira31@gmail.com>
2023-05-21 11:12:57 -0300

    Separeted file for messages
    For better redibility the messages displayed on help command will be
    under this file, now the main file is less poluted.

---
+++ init_template/messages.py
@@ -0,0 +1,23 @@
+PATH_HELP_MESSAGE = """
+(pt-BT) O caminho onde você deseja criar o projeto.
+
+EX ".", "./repo"
+
+"""
+
+TEMPLATE_HELP_MESSAGE = """
+(pt-BR) Qual template você deseja, o valor padrão é um projeto minimalista de TS
+"""
+
+ALIAS_HELP_MESSAGE = """
+(pt-BR) O alias que vocé deseja utilizar para o template do repositório, por convenção,
+adicione um "@" e o seu nickname do github seguido por "/" e o nome do template.
+
+Não utilizar espaços, subistitua espaço por "-".
+
+Ex: @EliasOlie/typescript-es2017
+"""
+
+TEMPLATE_ADD_HELP_MESSAGE = """
+(pt-BR) Link do repositório git contendo o template
+"""
